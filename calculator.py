452a3bfa4c6e42f678ea4a5747f90faf52dc6a79
Andres Neira <Andres.Neira@endava.com>
2026-02-13 11:44:46 -0500

    feat:substract functionality

--- calculator.py
+++ calculator.py
@@ -1,1 +1,7 @@
 print('-----Welcome to the Calculator----')
+
+def substract_two_numbers(a, b):
+    result = a - b
+    print(f'The result of your substraction is: {result}')
+
+substract_two_numbers(10,5)
